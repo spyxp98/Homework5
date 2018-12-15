1677079f39d1bf8433c92c3c3c6ee7ab51764fa3
Sereda Pavel <spyxp98@gmail.com>
2018-12-15 17:24:48 +0300

    final version

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from tqdm import *
 func1 = lambda x: (x**2)+math.sqrt(x-1)
 func1_int = lambda x: (x**3)/3 + (math.sqrt(x-1))**3 / (3/2)
 
-func2 = lambda x: x**2
-func2_int = lambda x: x**3 / 3
+func2 = lambda x: math.cos(21 * math.pi * x / 2)
+func2_int = lambda x: 2 /(21 *  math.pi) * math.sin(21 * math.pi * x / 2)
 
 def preload(filename):
    with open(f'{filename}', 'r') as f:
@@ -33,7 +33,7 @@ def legendre_deriv(n, x):
 # k-th root of polinomial of order n
 def legendre_root(k, n):
    return ((1-1/(8 * n**2) + 1/(8 * n **3)) 
-            * math.cos(math.pi * (4 * k -1)/(4 * n + 2)))
+            * math.cos(math.pi * (4 * k - 1)/(4 * n + 2)))
 
 
 
@@ -49,7 +49,8 @@ def legendre(func, func_int, eps):
          integral_sum += func(x_i) * a_i
       error = (abs((integral_sum - func_int(1) + func_int(-1))
                   /(func_int(1) - func_int(-1))))
-      n += 1
+      n += 5
+      print(f'leg - {n} - {error}')
       data.append(error)
    return data
       
@@ -72,12 +73,14 @@ def chebyshev(func, func_int, eps):
       integral_sum = 0
       for i in range(n):
          x_i = chebyshev_roots(n, i + 1)
-         a_i = sum((1/(m + 1) - 1/(m - 1)) * chebyshev_pol(m, x_i) / (1 * n/2) for m in range(2, n - 1, 2))
-         a_i += 2 * chebyshev_pol(0, x_i) / n
+         # a_i = sum((1/(m + 1) - 1/(m - 1)) * chebyshev_pol(m, x_i) / (1 * n/2) for m in range(2, n - 1, 2))
+         # a_i += 2 * chebyshev_pol(0, x_i) / n
+         a_i = (math.pi / n) * math.sin(math.pi * (i+1 - 0.5)/n)
          integral_sum += a_i * func(x_i)
       error = (abs((integral_sum - func_int(1) + func_int(-1))
                   /(func_int(1) - func_int(-1))))
       n += 1
+      print(f'cheb - {n} - {error}')
       data.append(error)
    return data
 
@@ -95,7 +98,10 @@ def trapez(func, func_int, a, b, eps):
       N *= 2
       step = (b - a)/N
       while curr_x < b:
-         integral_sum += step*(func(curr_x) + func(curr_x + step))/2
+         try:
+            integral_sum += step*(func(curr_x) + func(curr_x + step))/2
+         except ZeroDivisionError:
+            pass
          curr_x += step
       error = (abs((integral_sum - func_int(b) + func_int(a))
                   /(func_int(b) - func_int(a))))
@@ -113,11 +119,11 @@ def plot(filename):
          data1.append(float(temp[0]))
          data2.append(float(temp[1]))
          data3.append(float(temp[2]))
-   plt.scatter(epsilons[:10], data1, color = 'red', marker = 'o')
+   plt.scatter(epsilons[:7], data1, color = 'red', marker = 'o')
    plt.xscale('log')
-   plt.scatter(epsilons[:10], data2, color = 'green', marker = '^')
+   plt.scatter(epsilons[:7], data2, color = 'green', marker = '^')
    plt.xscale('log')
-   plt.scatter(epsilons[:10], data3, color = 'blue', marker = 'v')
+   plt.scatter(epsilons[:7], data3, color = 'blue', marker = 'v')
    plt.xscale('log')
    plt.legend(['Trapezium', 'Legendre', 'Chebyshev'])
    plt.xlabel('relative error')
@@ -129,7 +135,7 @@ def time_it(func, func_int):
    epsilons = preload('eps')
    with open('results', 'w') as f:
       # f.write('Трапеции, Лежандр, Чебышев \n')
-      for i in tqdm(range(11)): 
+      for i in tqdm(range(10)): 
          eps = epsilons[i]
          curr_time = time.time()
          data = trapez(func, func_int, -1, 1, eps)
@@ -150,5 +156,5 @@ if __name__ == '__main__':
    # data2 = legendre(func2, func2_int, eps)
    # print(data2)
    # plot(data1, data2)
-   time_it(func2, func2_int)
-   # plot('results')
+   # time_it(func2, func2_int)
+   plot('results')
